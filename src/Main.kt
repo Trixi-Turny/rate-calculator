09a911d126f98f97f44f9956febd9a33e7d5c4cd
Trixi Turny <trixi.turny@gmail.com>
2018-10-15 14:59:57 +0100

    add some validation and logic to main

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,48 @@
-import java.io.File
 import java.nio.file.FileSystems
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+
 class Main {
 
     val filePath: String = "../loan_files";
+    val fileName: String = "market";
 
-    val path: Path = FileSystems.getDefault().getPath(filePath, "market.txt");
+    fun getMimeType(filePath: String, fileName: String): String {
+        var mime :String = "";
 
-    val file: String = Files.probeContentType(path);
+        try {
+            val path: Path = FileSystems.getDefault().getPath(filePath, fileName)
+            mime = Files.probeContentType(path);
 
-    fun printIt() {
-        println(file);
+        } catch (e: IllegalStateException) {
+            println("Could not found file with name: "+fileName);
+        }
+        return mime
+    }
+
+    fun startProcess(filePath: String, fileName: String) {
+        var mime = "";
+        try {
+            val fileProcessorFactory: FileProcessorFactory? = FileProcessorFactory();
+            mime = this.getMimeType(filePath, fileName)
+            println(mime);
+            if(Validator.isValidEnum(mime)){
+                val fileProcessor: FileProcessor? = fileProcessorFactory?.getFileProcessor(FileType.valueOf(mime));
+                val loans : ArrayList<Loan> = fileProcessor.processFile(filePath+"/"+fileName);
+            }
+            else{
+                throw  IllegalArgumentException();
+            }
+        } catch (e: IllegalArgumentException) {
+            println("Unsupported File Type: "+ mime +". Currently supported files are :" )
+            FileType.values().forEach{println(it.name)};
+        }
+
     }
 }
 
-fun main (args : Array<String>) {
-    val init: Main = Main();
-    init.printIt();
-}
+    fun main(args: Array<String>) {
+        val init: Main = Main();
+        init.startProcess(init.filePath, init.fileName);
+    }
--- src/Validator.kt
+++ src/Validator.kt
@@ -3,4 +3,10 @@ object Validator {
     //String validator for filename
     //file exists
     //amount validator
+
+    fun isValidEnum(s:String): Boolean  = s in FileType.values().map{it.name};
+    fun isValidString(s: String): Boolean = s!=null;
+    fun isValidLoanAmount(n:Double, divider: Int):Boolean = n.rem(divider).equals(0);
+
+
 }
