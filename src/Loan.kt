1dba3389a8f08313921c257479137f8cb816eefe
Trixi Turny <trixi.turny@gmail.com>
2018-10-15 10:11:51 +0100

    create classes, plan strategy

---
+++ src/Loan.kt
@@ -0,0 +1,3 @@
+class Loan(val lender: String, val rate:Double, val availableAmount:Double) {
+
+}
---
+++ test/RateCalculatorTest.kt
@@ -0,0 +1,16 @@
+class RateCalculatorTest {
+
+
+    // input validation
+    // valid file
+    // valid amount
+    // it should give back the correct rates
+    // it should say no rates available when there are no loans
+    // it should give error message when amount is higher than allowed
+    // it should give error message if file does not exist or is empty
+    // it should give error message if any other file issue occurs
+    
+
+
+
+}
